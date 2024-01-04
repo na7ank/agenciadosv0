0a12713ca814bee12f3c5574313323c38683a068
bronzanat <natanbronzatto@gmail.com>
2024-01-04 16:55:45 -0300

    colors

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title="graficando",
                    )
 st.title('Informações Básicas sobre Agências Bancárias')
 
-hide_streamlit_style = """
+try_hidden = """
             <style>
             [data-testid="stToolbar"] {visibility: hidden !important;}
             #MainMenu {visibility: hidden !important;}
@@ -19,15 +19,20 @@ hide_streamlit_style = """
             a {display: none !important;}
             </style>
             """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+st.markdown(try_hidden, unsafe_allow_html=True)
 
 data = pd.read_csv('./dataset/202311bccags.csv', sep=';', encoding='utf-8-sig')
 
 # Side Menu
 with st.sidebar:
     st.write('Selecione os filtros de preferência para particionar os dados.')
-    check_box_ufs = st.checkbox('All UFs:' , value=False)
-    check_box_bancos = st.checkbox('All Bancos:' , value=False)
+
+    col1, col2 = st.columns(2)
+    with col1:
+        check_box_ufs = st.checkbox('All UFs:', value=False)
+    with col2:
+        check_box_bancos = st.checkbox('All Bancos:', value=False)
+
     uf = st.multiselect("UF", list(set(data['uf'])), ['SP'])
     instituicao = st.multiselect("Instituição", list(set(data['Instituição'])), ['BANCO BRADESCO S.A.'])
     codigo = st.slider('Agência Código', min(data['Código']), max(data['Código']), (0, 1000))
@@ -54,7 +59,9 @@ with bars:
     contagem_classes = data_full['Instituição'].value_counts().reset_index(name='Qnt')
     contagem_classes = contagem_classes.sort_values(by='Qnt', ascending=False).head(7)
     # Graph
-    fig = px.bar(contagem_classes, x='Qnt', y='Instituição', orientation='h', labels={'x': 'Qnt', 'y': 'Instituição'}, color='Qnt', color_continuous_scale='reds', opacity=0.8)
+    color = st.color_picker('', '#FF2600')
+    color_scale = [(0.0, '#EFEFEF'), (1.0, color)]
+    fig = px.bar(contagem_classes, x='Qnt', y='Instituição', orientation='h', labels={'x': 'Qnt', 'y': 'Instituição'}, color='Qnt', color_continuous_scale=color_scale, opacity=0.8)
     fig.update_layout(yaxis=dict(autorange="reversed"))
     st.plotly_chart(fig, config={"displayModeBar": False})
 
@@ -66,5 +73,3 @@ with locals:
     st.write("Locais com maior número de Agências.")
     groups = data[['Bairro', 'uf', 'Município']].groupby(['Bairro', 'uf', 'Município']).value_counts().reset_index(name='Qnt Agências')
     st.dataframe(groups, hide_index=True)
-
-#python -m streamlit run app.py
